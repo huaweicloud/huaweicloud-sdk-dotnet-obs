3206045e854e7021024ead6880e3af5315e54230
x00403408 <44666668+x00403408@users.noreply.github.com>
2019-04-18 13:45:06 +0800

    Add files via upload

--- Model/SetBucketTaggingRequest.cs
+++ Model/SetBucketTaggingRequest.cs
@@ -1,10 +1,22 @@
-
+/*----------------------------------------------------------------------------------
+// Copyright 2019 Huawei Technologies Co.,Ltd.
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License.  You may obtain a copy of the
+// License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations under the License.
+//----------------------------------------------------------------------------------*/
 using System.Collections.Generic;
 
 namespace OBS.Model
 {
     /// <summary>
-    /// ÉèÖÃÍ°±êÇ©µÄÇëÇó²ÎÊı¡£
+    /// è®¾ç½®æ¡¶æ ‡ç­¾çš„è¯·æ±‚å‚æ•°ã€‚
     /// </summary>
     public class SetBucketTaggingRequest : ObsBucketWebServiceRequest
     {
@@ -16,12 +28,12 @@ namespace OBS.Model
         }
 
         /// <summary>
-        /// Í°±êÇ©ÁĞ±í¡£
+        /// æ¡¶æ ‡ç­¾åˆ—è¡¨ã€‚
         /// </summary>
         /// <remarks>
         /// <para>
-        /// ²ÎÊı±ØÑ¡¡£
-        /// Ã¿¸öÍ°Ö§³Ö×î¶à10¸öTag¡£
+        /// å‚æ•°å¿…é€‰ã€‚
+        /// æ¯ä¸ªæ¡¶æ”¯æŒæœ€å¤š10ä¸ªTagã€‚
         /// </para>
         /// </remarks>
         public IList<Tag> Tags
